597d5fb1a70a1fd1f50d01512a86856b63142aee
Radus1k <mariusraduoaky@gmail.com>
2023-01-14 18:01:42 +0200

    created some templates

--- ebooking/hotels/views.py
+++ ebooking/hotels/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from hotels.models import Hotel
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseForbidden
+from hotels.models import Hotel, HotelRoom
+from .forms import ReservationForm
+from django.contrib import messages
+from .mail import send_mail
 
 def index(request):
     all_hotel_objs = Hotel.objects.all()
@@ -12,3 +15,33 @@ def add_hotel(request):
     # check if post request, check post data
     # if data valid, save object
     return render(request, "add_hotel.html", context={})
+
+def rent_rooms(request, hotel_id):
+    hotel_instance = Hotel.objects.get(id=hotel_id)
+
+    if not hotel_instance:
+        return HttpResponseForbidden
+
+    form = ReservationForm()
+    hotelRooms = HotelRoom.objects.filter(hotel=hotel_id)
+
+    context ={"form":form, "hotelrooms":hotelRooms}
+    email_context = {"rooms_rented": "10 rooms"}
+
+    if request.method=="POST":
+        form = ReservationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            send_mail("radumarius975@gmail.com","confirm_rent.html",context=email_context)
+        else:
+            messages.add_message(request, messages.ERROR, "Date introduse eronat!")
+            return render(request, template_name='rent_rooms.html', context=context)
+        messages.add_message(request, messages.SUCCESS, "Rezervare facuta cu succes!")
+        return redirect('home.html')
+    else:
+        return render(request, template_name='rent_rooms.html', context=context)
+
+
+
+
+
