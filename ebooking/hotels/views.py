7a195f5a348185952c67a03be5face1cde25e4f7
anamariaioanamarin <ana.marin3@s.unibuc.ro>
2023-04-10 16:39:50 +0300

    Update with remote changes

--- ebooking/hotels/views.py
+++ ebooking/hotels/views.py
@@ -45,3 +45,42 @@ def edit_rooms(request, hotel_id):
     return render(request, template_name='edit_rooms.html', context=context)
 
 
+def edit_room_view(request, hotel_id, room_id):
+    room = get_object_or_404(HotelRoom, id=room_id, hotel__id=hotel_id)
+
+    if request.method == 'POST':
+        form = HotelRoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('admin_hotel_rooms', hotel_id=hotel_id)
+    else:
+        form = HotelRoomForm(instance=room)
+
+    context = {'form': form, 'room': room, }
+    return render(request, 'edit_room.html',context=context)
+
+
+def add_room_view(request, hotel_id):
+    if request.method == 'POST':
+        form = AddHotelRoomForm(request.POST, request.FILES, user=request.user)
+        if form.is_valid():
+            hotelroom = form.save(commit=False)
+            hotel = Hotel.objects.get(id=hotel_id)
+            hotelroom.hotel = hotel
+            hotelroom.save()
+            messages.success(request, 'Hotel room added successfully.')
+            return redirect('admin_hotel_rooms', hotel_id=hotel_id)
+        else:
+             messages.error(request, 'Hotel room data incorrect.')
+    else:
+        form = AddHotelRoomForm(user=request.user)
+    return render(request, 'add_room.html', {'form': form})    
+
+def delete_room_view(request, hotel_id, room_id):
+    room = get_object_or_404(HotelRoom, id=room_id, hotel__id=hotel_id)
+    try:
+        room.delete()
+        messages.success(request, 'Room deleted successfully.')
+    except:
+        messages.error(request, 'Error deleting room.')
+    return redirect('admin_hotel_rooms', hotel_id=hotel_id)
